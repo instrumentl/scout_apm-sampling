c3e49e201ed608b5a2a16ab6d29507ee5303d276
Gabriel Sinkin <gabe@instrumentl.com>
2022-12-09 13:10:42 -0800

    feat(sidekiq): add a Sidekiq Server middleware for sampling

---
+++ lib/scout_apm/sampling/sidekiq/server_middleware.rb
@@ -0,0 +1,20 @@
+module ScoutApm
+  module Sampling
+    module Sidekiq
+      include Callbacks
+
+      class ServerMiddleware
+        # @param [Object] worker the worker instance
+        # @param [Hash] job the full job payload
+        #   * @see https://github.com/mperham/sidekiq/wiki/Job-Format
+        # @param [String] queue the name of the queue the job was pulled from
+        # @yield the next middleware in the chain or worker `perform` method
+        # @return [Void]
+        def call(worker, job, queue)
+          sample_workers_for_scout
+          yield
+        end
+      end
+    end
+  end
+end
