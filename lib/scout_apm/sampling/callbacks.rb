e9d0db2df56c99368fe105cb432b0d7485bddccd
Gabriel Sinkin <gabe@instrumentl.com>
2022-12-09 14:07:11 -0800

    fix: move sample methods to fully named due to mixin

--- lib/scout_apm/sampling/callbacks.rb
+++ lib/scout_apm/sampling/callbacks.rb
@@ -13,18 +13,26 @@ module ScoutApm
         ENV["SCOUT_APM_WORKER_SAMPLING_RATE"].presence.try(:to_f) || SCOUT_APM_SAMPLING_RATE
       ).abs
 
-      private
-
-      def sample_requests_for_scout
+      def self.sample_requests_for_scout
         if rand >= SCOUT_APM_SAMPLING_RATE
           ScoutApm::Transaction.ignore!
         end
       end
 
-      def sample_workers_for_scout
+      def self.sample_workers_for_scout
         if rand >= SCOUT_APM_WORKER_SAMPLING_RATE
           ScoutApm::Transaction.ignore!
         end
+      end
+
+      private
+
+      def sample_requests_for_scout
+        ScoutApm::Sampling::Callbacks.sample_requests_for_scout
+      end
+
+      def sample_workers_for_scout
+        ScoutApm::Sampling::Callbacks.sample_workers_for_scout
       end
     end
   end
